68a67fdc7eb8c11739c0a627af4f11eecdf6a503
Michael Klatt <mdklatt@alumni.ou.edu>
2019-05-20 23:29:06 -0500

    Moved all project source files to src/ directory

--- {{cookiecutter.project_slug}}/rollup.config.js
+++ {{cookiecutter.project_slug}}/rollup.config.js
@@ -1,19 +1,44 @@
+import fs from 'fs-extra';
+import path from 'path';
+import resolve from 'rollup-plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
 import typescript from 'rollup-plugin-typescript'
-import resolve from 'rollup-plugin-node-resolve'
+
 
 const develop = process.env.ROLLUP_WATCH
 
+
+/**
+ * Plugin for copying static files to their output directory.
+ *
+ * @param options copy options
+ * @returns
+ */
+const copy = function(options) {
+    return {
+        generateBundle() {
+            const root = path.dirname(options.dest)
+            fs.ensureDirSync(root)
+            fs.copy(options.src, options.dest)
+        }
+    }
+}
+
+
 export default {
-	input: 'src/main.ts',
+	input: 'src/script/main.ts',
 	output: {
-		file: 'static/script/main.min.js',
+		file: 'static/script/main.js',
 		format: 'esm',
 		sourcemap: true
 	},
 	plugins: [
+        copy({
+            src:  'src/style/',
+            dest: 'static/style/'
+        }),
 		typescript(),
 		resolve(),  // locate packages in node_modules
 		!develop && terser(),  // minify in production
 	]
-};
+}
